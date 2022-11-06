49fdc6baf140ee46685fcc98e588105ea3598ff0
nazaro <nazaro@tamu.edu>
2022-11-05 23:21:30 -0500

    Added server side route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Inventory from './components/Inventory';
 import ViewEditMenu from './components/ViewEditMenu';
 import ManagerSide from './components/ManagerSide';
 import RestockReport from './components/RestockReport';
+import ServerSide from './components/ServerSide';
 import Login from './components/Login';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
             <Route path='/' element={<Login/>}/>
             <Route path='/Inventory' element={<Inventory/>}/>
             <Route path='/ViewEditMenu' element={<ViewEditMenu/>}/>
+            <Route path='/ServerSide' element={<ServerSide/>}/>
           </Routes>
         </BrowserRouter>
       </>
