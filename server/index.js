bbe29fea6cc6aacbf6ec43890220fd56b7735bf4
WaylandMoody55 <90230570+WaylandMoody55@users.noreply.github.com>
2022-11-21 13:54:28 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -26,8 +26,9 @@ process.on('SIGINT', function() {
   console.log('Application successfully shutdown');
   process.exit(0);
 });
-app.use((req, res) => {
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  next();
 });
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
